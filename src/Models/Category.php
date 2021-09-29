6ca75db884c2c295545c61afb833a1004daf6d1b
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2021-09-29 15:09:54 +0300

    fix 2

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    //protected $table = 'categories';
-
     public function posts()
     {
         return $this->hasMany(Post::class);
